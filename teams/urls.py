ab6ce6921e108193ddcf95ae8e8a5ef22f5dcb62
Devraj Shetake <devrajshetake@gmail.com>
2022-08-28 15:45:03 +0530

    ashjh

--- teams/urls.py
+++ teams/urls.py
@@ -18,8 +18,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('teams/', teams, name="teams1"),
-    path('teams1/', teams, name="teams2"),
+    path('teams/', teams, name="teams"),
     path('teams/<int:pk>', team, name="team"),
     path('players/', players, name="players"),
     path('players/<int:pk>', player, name="player"),
